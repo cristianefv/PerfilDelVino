61640f8c271ae46f8bd2df0a22f19fa0cad9dcb1
Cristian Flores <efvcristian@gmail.com>
2022-06-27 20:02:31 -0300

    indentada la tienda.js

--- JavaScript/tienda.js
+++ JavaScript/tienda.js
@@ -22,53 +22,50 @@ function comprar() {
     let continuar = true;
     alert("Bienvenido a la tienda de vinos de 'Perfil del vino'");
 
+    while (continuar) {
+        let producto = prompt(`
+        Seleccione el id que corresponda al vino que desee.
 
-while (continuar) {
-    let producto = prompt(`
-    Seleccione el id que corresponda al vino que desee.
+        Lista de vinos:
+        ${vino1.id}) ${vino1.etiqueta}, ${vino1.varietal}, ${vino1.anada} - $${vino1.precio} 
+        ${vino2.id}) ${vino2.etiqueta}, ${vino2.varietal}, ${vino2.anada} - $${vino2.precio} 
+        ${vino3.id}) ${vino3.etiqueta}, ${vino3.varietal}, ${vino3.anada} - $${vino3.precio} 
+        ${vino4.id}) ${vino4.etiqueta}, ${vino4.varietal}, ${vino4.anada} - $${vino4.precio} 
+        ${vino5.id}) ${vino5.etiqueta}, ${vino5.varietal}, ${vino5.anada} - $${vino5.precio}  
 
-    Lista de vinos:
-    ${vino1.id}) ${vino1.etiqueta}, ${vino1.varietal}, ${vino1.anada} - $${vino1.precio} 
-    ${vino2.id}) ${vino2.etiqueta}, ${vino2.varietal}, ${vino2.anada} - $${vino2.precio} 
-    ${vino3.id}) ${vino3.etiqueta}, ${vino3.varietal}, ${vino3.anada} - $${vino3.precio} 
-    ${vino4.id}) ${vino4.etiqueta}, ${vino4.varietal}, ${vino4.anada} - $${vino4.precio} 
-    ${vino5.id}) ${vino5.etiqueta}, ${vino5.varietal}, ${vino5.anada} - $${vino5.precio}  
-    
-    6) Terminar compra
-    
-    Total de la compra: $${costoTotal}`);
+        6) Terminar compra
 
+        Total de la compra: $${costoTotal}`);
 
-switch (producto) {
-    case "1":
-        sumar(vino1.precio);
-        break;
-    case "2":
-        sumar(vino2.precio);
-        break;
-    case "3":
-        sumar(vino3.precio);
-        break;
-    case "4":
-        sumar(vino4.precio);
-        break;
-    case "5":
-        sumar(vino5.precio);
-        break;
-    case "6":
-        continuar = false;
-        break;
-    default:
-        alert("El id ingresado no corresponde a alguno de los productos");
-        break;
+        switch (producto) {
+            case "1":
+                sumar(vino1.precio);
+                break;
+            case "2":
+                sumar(vino2.precio);
+                break;
+            case "3":
+                sumar(vino3.precio);
+                break;
+            case "4":
+                sumar(vino4.precio);
+                break;
+            case "5":
+                sumar(vino5.precio);
+                break;
+            case "6":
+                continuar = false;
+                break;
+            default:
+                alert("El id ingresado no corresponde a alguno de los productos");
+                break;
+        }
+
+        if (producto !=6) continuar = confirm("Desea seguir comprando?");
     }
 
-
-if (producto !=6) continuar = confirm("Desea seguir comprando?");
-}
-
-alert(`El monto total a abonar es: $${costoTotal}`);
-alert(`Muchas gracias por tu compra!`)
+    alert(`El monto total a abonar es: $${costoTotal}`);
+    alert(`Muchas gracias por tu compra!`)
 
 }
 
